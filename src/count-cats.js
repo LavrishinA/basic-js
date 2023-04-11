5848a1a601cd1c0cda169bc3cdc36522fe604458
LavrishinA <iHasher007@gmail.com>
2023-04-12 02:35:24 +0300

    sloved count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+    return (catsCount = matrix
+        .map((elem) => elem.filter((elem) => elem === "^^"))
+        .reduce((sum, cur) => sum + cur.length, 0));
 }
 
 module.exports = {
-  countCats
+    countCats,
 };
