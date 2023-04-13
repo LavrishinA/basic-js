cf4fc85154e600d7bf88d92581788a4b5c9889b4
LavrishinA <iHasher007@gmail.com>
2023-04-13 21:45:21 +0300

    solved dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,27 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+    if (!Array.isArray(arr)) return false;
+    return arr
+        .filter((elem) => typeof elem === "string")
+        .map((elem) => elem.trim().slice(0, 1).toUpperCase())
+        .sort((a, b) => a.localeCompare(b))
+        .join("");
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam,
 };
