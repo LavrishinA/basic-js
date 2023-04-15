a13c0d17f9b0defeff1edc57dad44986aa263a16
LavrishinA <iHasher007@gmail.com>
2023-04-15 21:58:10 +0300

    solved sun-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+    let sum = String(n)
+        .split("")
+        .reduce((prev, cur) => +(+prev) + +cur, 0);
+    if (String(sum).length !== 1) sum = getSumOfDigits(sum);
+    return sum;
 }
 
 module.exports = {
-  getSumOfDigits
+    getSumOfDigits,
 };
